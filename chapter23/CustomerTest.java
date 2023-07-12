6e88c772c504d1b887178af3282afed62a0393a3
seungo ha <seungo.ha@actionpower.kr>
2023-07-13 07:45:28 +0900

    Revert "feat: OOP test"
    This reverts commit 0bd771d453408300d6807b64211a016b07dcf33f.

--- chapter23/CustomerTest.java
+++ chapter23/CustomerTest.java
@@ -1,26 +1,18 @@
 package chapter23;
 
 public class CustomerTest {
-
     public static void main(String[] args) {
-        int price = 10000;
-
         Customer customerLee = new Customer();
         customerLee.setCustomerId(1000);
         customerLee.setCustomerName("이순신");
         customerLee.bonusPoint = 1000;
         customerLee.showCustomerInfo();
-        int costForLee = customerLee.calcPrice(price);
-        System.out.println("금액은 " + costForLee + " 입니다.");
 
-        VipCustomer customerHa = new VipCustomer(1001, "하승오");
-        Customer customerO = new VipCustomer(1002, "오지수"); // type-casting (묵시적 형 변환이 가능)
-
+        VipCustomer customerHa = new VipCustomer();
+        customerHa.setCustomerId(1001);
+        customerHa.setCustomerName("하승오");
         customerHa.bonusPoint = 1500;
         customerHa.showCustomerInfo();
-
-        int costForHa = customerHa.calcPrice(price);
-        System.out.println("금액은 " + costForHa + " 입니다.");
 
     }
 }
--- chapter23/VipCustomer.java
+++ chapter23/VipCustomer.java
@@ -1,26 +1,12 @@
 package chapter23;
 
 public class VipCustomer extends Customer {
-    private int agentId = 0;
+    private int agentId;
     double salesRatio;
 
-    public VipCustomer(int customerId, String customerName) {
-        System.out.println("VipCustomer() invoked");
-        this.customerId = customerId;
-        this.customerName = customerName;
+    public VipCustomer() {
         customerGrade = "VIP";
         salesRatio = 0.05;
         bonusRatio = 0.1;
-    }
-
-    @Override
-    public int calcPrice(int price) {
-        bonusPoint += price * bonusRatio;
-        return price - (int)(price * salesRatio);
-    }
-@Override
-    public void showCustomerInfo(){
-        super.showCustomerInfo();
-        System.out.println("상담원 아이디는 " + agentId + " 입니다.");
     }
 }
