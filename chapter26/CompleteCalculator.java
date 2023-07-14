8899389b03b5ebfd080eaa5500ed5bf5aae5f5af
seungo <ilkhso@gmail.com>
2023-07-14 23:37:47 +0900

    feat: interface

---
+++ chapter26/CompleteCalculator.java
@@ -0,0 +1,13 @@
+package chapter26;
+
+public class CompleteCalculator extends Calculator {
+    @Override
+    public int times(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
